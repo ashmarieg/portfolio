4145cb2636ff904eac14419864eb59c5f029f032
ashmarieg <ashleymglenny@gmail.com>
2021-07-12 14:57:09 -0400

    commit about component responsiveness

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import './App.css';
 import NavBar from './components/NavBar/NavBar'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 import Home from './pages/Home'
-import Contact from './components/Contact/Contact'
-import About from './components/About/About'
-import Projects from './components/Projects/Projects';
-import Header from './components/Header/Header'
+
 
 function App() {
   return (
-    <Router>
-    <div className="App">
-      {/* <NavBar /> */}
-      {/* <Header /> */}
+    
+    <div className="App">  
       <Home />
-      {/* <About />
-      <Projects />
-      <Contact /> */}
-      
     </div>
-    </Router>
+   
   );
 }
 
